4fee911ec8d1cc9e03e5e089e4e6256bee05604f
Max <qiwi360@gmail.com>
2015-10-30 16:59:36 +0300

    + profile.clj added

---
+++ dmdp/src/dmdp/dmdp/profile.clj
@@ -0,0 +1,31 @@
+(ns dmdp.dmdp.profile
+  (:require [dmdp.layout :as layout]
+            [ring.util.response :refer [response redirect]]
+            [dmdp.db.core :as db]
+            [dmdp.dmdp.validators :as validators]))
+
+(defn profile-page [{:keys [params session]}]
+  (let [id (:id (:identity session nil) nil)]
+    (if (not= id nil)
+      (layout/render
+         "profile/profile.html" {:user (first (db/get-user {:id id}))
+         ;:publications (db/get-publications-by-author {:author_id (Integer/parseInt (:id params))})}))
+                           })
+      (redirect "/auth/login"))))
+
+(defn edit-profile-page [{:keys [params session]}]
+  (let [id (:id (:identity session nil) nil)]
+    (if (not= id nil)
+      (layout/render
+         "profile/edit_profile.html" {:user (first (db/get-user {:id (:id (:identity session))}))})
+      (redirect "/auth/login"))))
+
+(defn edit-profile! [{:keys [params session]}]
+  (if (validators/validate-edition params)
+    (do
+      (db/update-user! {:id (:id (:identity session))
+                        :first_name (:first_name params)
+                        :last_name (:last_name params)
+                        :email (:email params)})
+      (redirect "/profile"))
+    (redirect "/profile/edit")))
