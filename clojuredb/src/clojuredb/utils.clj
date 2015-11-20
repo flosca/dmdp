cc23db228dc9897384f660f8c4b4eba2bf6948b4
Max <qiwi360@gmail.com>
2015-11-20 23:59:34 +0300

    + calc-record-hash function

--- clojuredb/src/clojuredb/utils.clj
+++ clojuredb/src/clojuredb/utils.clj
@@ -24,3 +24,19 @@
 (if (.exists (io/as-file file))
 (io/delete-file file)
 (println "File does not exist yet."))))
+
+(defn shift-file
+  [filename pos-start count]
+    (with-open [raf (new java.io.RandomAccessFile filename "rwd")]
+      (loop [i (dec (.length raf))
+             pos-read (dec (.length raf))
+             pos-write (dec (+ (.length raf) count))]
+            (if (>= i pos-start)
+                (do
+                  (printf "pr: %s, pw: %s; " pos-read pos-write)
+                  (let [current-byte (do (.seek raf pos-read)
+                                         (.read raf))]
+                    (.seek raf pos-write)
+                    (.write raf current-byte))
+                  (recur (dec i) (dec pos-read) (dec pos-write)))))
+      (.setLength raf (+ (.length raf) (if (<= count 0) count 0) ))))
